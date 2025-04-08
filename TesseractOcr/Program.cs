c526b21d0cfd00b5343f963bd7e45b2a740f5320
leventkalkavan <levent.kalkavan@hotmail.com>
2025-04-08 18:16:17 +0300

    tesseract

---
+++ TesseractOcr/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tesseract;
+
+Console.WriteLine("karakter okumasi yapilacak yol: ");
+string imagePath = Console.ReadLine();
+string tessData = @"/desktop/tessdata";
+try
+{
+    using (var engine = new TesseractEngine(tessData, "eng", EngineMode.Default))
+    {
+        using (var img = Pix.LoadFromFile(imagePath))
+        {
+            using (var page = engine.Process(img))
+            {
+                string pageText = page.GetText();
+                Console.WriteLine("resimden okunan text: ", pageText);
+            }
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"hata: {e.Message}");
+}
+Console.ReadLine();
